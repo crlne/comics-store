9b6c9d01e3a9e4fefec428879580c214802c7138
Caroline Sinigaglia <crlrne.ferreira@gmail.com>
2024-05-27 01:11:55 -0300

    Remove Vercel configuration and add GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    public: 'http://192.168.18.128:8080',
-    disableHostCheck: true,
-  }
-};
+  publicPath: process.env.NODE_ENV === 'production' ? '/<comics-store>/' : '/'
+}
