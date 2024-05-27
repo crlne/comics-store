a0a48558268920a5adbfe524d5720a594e45db32
Caroline Sinigaglia <crlrne.ferreira@gmail.com>
2024-05-27 01:26:29 -0300

    Deploy para o GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/<comics-store>/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/<comics-store>/' : '/',
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080
+  }
 }
