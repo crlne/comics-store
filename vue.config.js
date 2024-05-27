f37ad5de5083f610903217b44672d35c353e2332
Caroline Sinigaglia <crlrne.ferreira@gmail.com>
2024-05-27 00:55:42 -0300

    Update Vercel deployment configuration

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
-const { defineConfig } = require('@vue/cli-service');
-
-module.exports = defineConfig({
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   devServer: {
     host: '0.0.0.0',
     port: 8080,
     public: 'http://192.168.18.128:8080',
     disableHostCheck: true,
-  },
-});
-
+  }
+};
